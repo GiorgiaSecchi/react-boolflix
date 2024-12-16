93365f7f94bd435565f31f7b331107a2df60a4c8
GiorgiaSecchi <secchigrg@gmail.com>
2024-12-16 14:37:03 +0100

    add Searchbar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,22 @@
 import { NavLink } from "react-router-dom";
 
 export default function Searchbar() {
-  return <></>;
+  return (
+    <nav className="navbar bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand">BOOLFIX</a>
+        <form className="d-flex" role="search">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-danger" type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+    </nav>
+  );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <div className="container py-5">
-      <h1>Prova a cercare un film o una serie tv!</h1>
+      <h3 className="text-center">Prova a cercare un film o una serie tv!</h3>
     </div>
   );
 }
