0c4e84454a558f66e7e6a0040fe8734dbefab901
GiorgiaSecchi <secchigrg@gmail.com>
2024-12-16 17:46:36 +0100

    add MovieList

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+
+export default function MovieList() {
+  return (
+    <ul class="list-group">
+      <li class="list-group-item">An item</li>
+      <li class="list-group-item">A second item</li>
+      <li class="list-group-item">A third item</li>
+      <li class="list-group-item">A fourth item</li>
+      <li class="list-group-item">And a fifth one</li>
+    </ul>
+  );
+}
